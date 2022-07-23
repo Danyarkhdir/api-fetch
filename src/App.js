6cb65b7e6142536977dd0b501f43f2d07b0ae3b0
Danyar Khdir <daniyar.khdir@gmail.com>
2022-07-23 19:44:56 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import NavBar from "./components/NavBar";
 function App() {
-  return <div className="text-yellow-600">Danyar Khdir</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
